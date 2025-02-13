c01387e6787d90b0bd115b397dc760d89b8a2691
123alexandra123 <satalanalexa@gmail.com>
2025-02-13 18:22:52 +0200

    last commit

--- mutare_fisiere_in_cloud.sh
+++ mutare_fisiere_in_cloud.sh
@@ -11,25 +11,25 @@ mutare_fisiere_in_cloud() {
         return 1
     fi
 
-    # verifica daca directorul 'repo' exista; daca nu, il creeaza si configureaza git
-    if [[ ! -d "repo" ]]; then
-        echo "Directorul 'repo' nu exista. Creare..."
-        mkdir repo                           # creeaza directorul 'repo'
-        cd repo                             # schimba directorul curent in 'repo'
-        git init                            # initializeaza un repository git in 'repo'
-        git remote add origin https://github.com/123alexandra123/my-repo.git # adauga remote-ul github
+    # verifica daca directorul 'my-repo' exista; daca nu, il creeaza si configureaza git
+    if [[ ! -d "my-repo" ]]; then
+        echo "Directorul 'my-repo' nu exista. Creare..."
+        mkdir my-repo                           # creeaza directorul 'my-repo'
+        cd my-repo                             # schimba directorul curent in 'my-repo'
+        git init                            # initializeaza un my-repository git in 'my-repo'
+        git remote add origin https://github.com/123alexandra123/my-my-repo.git # adauga remote-ul github
     else
-        cd repo                             # schimba directorul curent in 'repo' daca exista deja
+        cd my-repo                             # schimba directorul curent in 'my-repo' daca exista deja
     fi
 
     # verifica daca remote-ul github este configurat; daca nu, il configureaza
     remote_url=$(git remote get-url origin 2>/dev/null)
     if [[ -z "$remote_url" ]]; then
         echo "Remote-ul GitHub nu este configurat. Configurare..."
-        git remote add origin https://github.com/123alexandra123/my-repo.git
+        git remote add origin https://github.com/123alexandra123/my-my-repo.git
     fi
 
-    # muta fisierul specificat de utilizator in directorul 'repo'
+    # muta fisierul specificat de utilizator in directorul 'my-repo'
     mv "../$file" .
 
     # adauga fisierul in zona de staging a git
@@ -38,14 +38,14 @@ mutare_fisiere_in_cloud() {
     # creeaza un commit cu mesajul care descrie adaugarea fisierului
     git commit -m "Adaugat fisierul $file"
 
-    # trimite commit-ul catre repository-ul remote de pe github
+    # trimite commit-ul catre my-repository-ul remote de pe github
     if ! git push; then
         echo "Eroare la trimiterea catre GitHub. Verificati autentificarea."
         return 1
     fi
 
     # afiseaza un mesaj de succes daca fisierul a fost trimis cu succes
-    echo "Fisierul '$file' a fost mutat si trimis cu succes in repository-ul GitHub."
+    echo "Fisierul '$file' a fost mutat si trimis cu succes in my-repository-ul GitHub."
     return 0
 }
 
